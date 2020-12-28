26b547543e32a28419b730e507098fa5931e3505
kanno <m.kanno.0915@gmail.com>
2020-12-28 15:03:54 +0900

    implement Mult by assembly

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// init
+@count
+M=0
+@R2
+M=0
+
+(LOOP)
+// check count
+@count
+D=M
+@R1
+D=M-D
+@END
+D;JLE
+
+// add
+@R0
+D=M
+@R2
+M=M+D
+
+// increment count
+@count
+M=M+1
+
+@LOOP
+0;JMP
+(END)
